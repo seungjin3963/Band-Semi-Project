bf19f99a6bd9450b7f0c685cd211b463ed4aab4b
gyujinpark <gyujinpark@localhost>
2020-05-16 17:23:29 +0900

    first Commit

---
+++ Band-Semi-Project/src/jhta/band/controller/home/HomeController.java
@@ -0,0 +1,19 @@
+package jhta.band.controller.home;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home.do")
+public class HomeController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String contextPath = req.getContextPath();
+		req.getServletContext().setAttribute("cp", contextPath);
+		resp.sendRedirect(req.getContextPath()); // login Page move!!
+	}
+}
