d69ce6e835b604bd75094a89b083c2fc0c5ce173
JHTA <JHTA@JHTA-PC>
2020-05-22 15:48:50 +0900

    random view update

--- Band-Semi-Project/src/jhta/band/bandSerch/Dao/bandDao.java
+++ Band-Semi-Project/src/jhta/band/bandSerch/Dao/bandDao.java
@@ -139,16 +139,41 @@ public class bandDao {
 			JDBCUtil.close(rs, pstmt, con);
 		}	
 	}
+	public ArrayList<Integer> getBandNum() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		try {
+			con=JDBCUtil.getConn();
+			String sql="select band_num "
+					+ "from band where band_publicwhe!=3 rownum<10 and rownum>=0"
+					+ "order by dbms_random.value ";
+			
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				int band_num=rs.getInt("band_num");
+				list.add(band_num);
+			}
+			return list;
+		}catch(SQLException se) {
+			System.out.println(se.getMessage());
+			return null;
+		}finally {
+			JDBCUtil.close(rs, pstmt, con);
+		}
+	}
 	public ArrayList<bandVo> random(){
 		
 		String sql="";
 		Connection con=null;
 		PreparedStatement pstmt=null;
-		PreparedStatement pstmt2=null;
 		ResultSet rs=null;
-		ResultSet rs2=null;
 		int a[] = new int[10]; 
-        Random r = new Random(); 
+        Random r = new Random();
+       // ArrayList<Integer> list1=getBandNum();
+        
         for(int i=0;i<=9;i++){
             a[i] = r.nextInt(10)+1; 
             for(int j=0;j<i;j++){//?—¬ê¸°ì—?„œ ë°´ë“œê³µê°œ?—¬ë¶??„ ?Œ?‹¨?•´?„œ iê°ì‚°?•˜ê¸?
@@ -161,11 +186,12 @@ public class bandDao {
 		ArrayList<bandVo> list=new ArrayList<bandVo>();
 		 //sql="select * from band1 where band_num>0 and band_num<11";
 		 //sql="select * from band1 where band_num in(?,?,?,?,?,?,?,?,?,?) and band_publicwhe!=3";
-		 sql="select * from band where band_num in(?,?,?,?,?,?,?,?,?,?) order by dbms_random.value";
-		try {
+		 //sql="select * from band where band_num in(?,?,?,?,?,?,?,?,?,?) order by dbms_random.value";
+		 String sql2="select aa.* from(select * from band where band_publicwhe!=3  order by dbms_random.value )aa where rownum<=10 and rownum>=1";
+		 try {
 			con=JDBCUtil.getConn();
-			pstmt=con.prepareStatement(sql);
-			pstmt.setInt(1,a[0]);
+			pstmt=con.prepareStatement(sql2);
+	/*		pstmt.setInt(1,a[0]);
 			pstmt.setInt(2,a[1]);
 			pstmt.setInt(3,a[2]);
 			pstmt.setInt(4,a[3]);
@@ -174,7 +200,7 @@ public class bandDao {
 			pstmt.setInt(7,a[6]);
 			pstmt.setInt(8,a[7]);
 			pstmt.setInt(9,a[8]);
-			pstmt.setInt(10,a[9]);
+			pstmt.setInt(10,a[9]);*/
 			rs=pstmt.executeQuery();
 			while(rs.next()) {
 				int band_num=rs.getInt("band_num");
