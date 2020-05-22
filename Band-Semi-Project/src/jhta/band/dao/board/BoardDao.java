83427a0e246f24c2aac3020448c868f081652350
gyujinpark <gyujinpark@localhost>
2020-05-22 15:11:01 +0900

    board updload comments upload SQLupdate

--- Band-Semi-Project/src/jhta/band/dao/board/BoardDao.java
+++ Band-Semi-Project/src/jhta/band/dao/board/BoardDao.java
@@ -149,8 +149,11 @@ public class BoardDao {
 		ResultSet rs = null;
 		try {
 			con = JDBCUtil.getConn();
-			String sql = "select aa.*,bb.band_nickname from board aa,(select band_nickname from band_userinfo) bb where band_num=? and" + 
-					"board_states!=2 order by board_redate DESC";
+			String sql = "select aa.*,band_nickname from band_userinfo bb,\n" + 
+					"(\n" + 
+					"    select * from board where band_num=? and board_states!=2 order by board_redate DESC\n" + 
+					")aa\n" + 
+					"where aa.userband_num=bb.userband_num";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, band_num);
 						
